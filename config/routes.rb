cf31e8da1dd9e0fd437786252488c9fdecefc4dc
jsd <rails -v>
2014-10-29 02:00:48 +0500

    DB setting again

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  root 'player#index'
 
   resources :players do
     resources :readings
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'player#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
